b8d6745e84fefcae7542767b57b4c67117ab7511
Admin <sandhyabagale10@gmail.com>
2021-12-28 21:21:23 +0530

    Added:Employee details are added

--- JavaScript/employeepayroll.js
+++ JavaScript/employeepayroll.js
@@ -4,35 +4,22 @@ window.addEvenListener('DOMContentLoaded',(event) =>{
 
 //Templete features 
 const createInnerHtml = () =>{
-const innerHtml =`
-<table id="display" class="table-main">
-    <!-- UC 3 -->
-    <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Gender</th>
-        <th>Department</th>
-        <th>Salary</th>
-        <th>Start Date</th>
-        <th>Actions</th>
-    </tr>
-    <tr>
-        <td><img class="profile" alt="" src="Asset/Profile pic/Ellipse -2.png">
-        </td>
-        <td>Narayan Mahadevan</td>
-        <td>Male</td>
-        <td>
-            <div class="dept-label">HR</div>
-            <div class="dept-label">Finance</div>
-        </td>
-        <td>3000000</td>
-        <td>1 Nov 2020</td>
-        <td>
-            <img id="1" onclick="remove(this)" alt="delete" src="Asset/icons/delete-black-18dp.svg">
-            <img id="1" alt="edit" onclick="update(this)" src="Asset/icons/create-black-18dp.svg">
-        </td>
-    </tr>
-</table>
+const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
+                    "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+
+const innerHtml = `'${headerHtml}
+<tr>
+    <td><img class="profile" src="Asset/Profile pic/Ellipse -2.png" alt=""></td> 
+    <td>Narayan Mahadevan</td>
+    <td>Male</td>
+    <td><div class="dept-label">HR<</div><div class='dept-label'>Finance</div></td> 
+    <td>3000000</td>
+    <td>1 Nov 2020</td>
+    <td>
+    <img name="1" onclick="remove(this)" src="Asset/icons/delete-black-18dp.svg" alt="delete">
+    <img name="1" onclick="update(this)" src="Asset/icons/create-black-18dp.svg" alt="edit">
+</td>
+</tr>
 `;
 document.querySelector('#table-display').innerHtml = innerHtml;
 }
